5f4d64f32670da7aa3b3b6cb230462e774d374d2
Stratos Papadoudis <stratos.papadoudis@helvia.ai>
2026-08-03 20:43:13 +0300

    Upgrade concrete numeric classes

--- src/addons/base.py
+++ src/addons/base.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 
 import math
+import operator
 import typing
 
 
@@ -20,17 +21,17 @@ class Int:
 	def __int__  (self) -> int  : return int  (self._)
 	def __float__(self) -> float: return float(self._)
 
-	def __add__(self, other: typing.Self) -> typing.Self: return self.operation(other, operation = int.__add__)
-	def __mul__(self, other: typing.Self) -> typing.Self: return self.operation(other, operation = int.__mul__)
+	def __add__(self, other: typing.Self, /) -> typing.Self: return self.operator(other, operator = operator.add)
+	def __mul__(self, other: typing.Self, /) -> typing.Self: return self.operator(other, operator = operator.mul)
 
-	def __pos__(self) -> typing.Self: return self.operation(operation = int.__pos__)
-	def __abs__(self) -> typing.Self: return self.operation(operation = int.__abs__)
+	def __pos__(self) -> typing.Self: return self.operator(operator = operator.pos)
+	def __abs__(self) -> typing.Self: return self.operator(operator = operator.abs)
 
-	def __eq__(self, other: object) -> bool: return self.comparison(other, int.__eq__)
-	def __le__(self, other: typing.Self) -> bool: return self.comparison(other, int.__le__)
-	def __ge__(self, other: typing.Self) -> bool: return self.comparison(other, int.__ge__)
-	def __lt__(self, other: typing.Self) -> bool: return self.comparison(other, int.__lt__)
-	def __gt__(self, other: typing.Self) -> bool: return self.comparison(other, int.__gt__)
+	def __eq__(self, other: object     , /) -> bool: return self.relation(other, relation = operator.eq)
+	def __le__(self, other: typing.Self, /) -> bool: return self.relation(other, relation = operator.le)
+	def __ge__(self, other: typing.Self, /) -> bool: return self.relation(other, relation = operator.ge)
+	def __lt__(self, other: typing.Self, /) -> bool: return self.relation(other, relation = operator.lt)
+	def __gt__(self, other: typing.Self, /) -> bool: return self.relation(other, relation = operator.gt)
 
 	@classmethod
 	def cast(cls, value: typing.Self | int) -> typing.Self:
@@ -57,26 +58,27 @@ class Int:
 		)
 
 	@typing.overload
-	def operation(self, /, *, operation: typing.Callable[[int], int]) -> typing.Self: ...
+	def operator(self, /, *, operator: typing.Callable[[int], int]) -> typing.Self: ...
 
 	@typing.overload
-	def operation(self, other: typing.Self, /, *, operation: typing.Callable[[int, int], int]) -> typing.Self: ...
+	def operator(self, other: typing.Self, /, *, operator: typing.Callable[[int, int], int]) -> typing.Self: ...
 
-	def operation(self, other: typing.Self | None = None, /, *, operation: typing.Callable[..., int]) -> typing.Self:
-		if other is None:
-			return self.cast(operation(self._))
+	def operator(self, *others: typing.Self, operator: typing.Callable[..., int]) -> typing.Self:
+		if not others:
+			return self.cast(operator(self._))
+
+		other, *_ = others
 
 		if type(other) is not type(self):
 			return NotImplemented
 
-		return self.cast(operation(self._, other._))
+		return self.cast(operator(self._, other._))
 
-	def comparison(self, other: object, comparison: typing.Callable[[int, int], bool]) -> bool:
+	def relation(self, other: object, relation: typing.Callable[[int, int], bool]) -> bool:
 		if type(other) is not type(self):
 			return NotImplemented
 
-		other = typing.cast(typing.Self, other)
-		return comparison(self._, other._)
+		return relation(self._, other._)
 
 
 class Float:
@@ -94,17 +96,17 @@ class Float:
 	def __int__  (self) -> int  : return int  (self._)
 	def __float__(self) -> float: return float(self._)
 
-	def __add__(self, other: typing.Self) -> typing.Self: return self.operation(other, operation = float.__add__)
-	def __mul__(self, other: typing.Self) -> typing.Self: return self.operation(other, operation = float.__mul__)
+	def __add__(self, other: typing.Self, /) -> typing.Self: return self.operator(other, operator = operator.add)
+	def __mul__(self, other: typing.Self, /) -> typing.Self: return self.operator(other, operator = operator.mul)
 
-	def __pos__(self) -> typing.Self: return self.operation(operation = float.__pos__)
-	def __abs__(self) -> typing.Self: return self.operation(operation = float.__abs__)
+	def __pos__(self) -> typing.Self: return self.operator(operator = operator.pos)
+	def __abs__(self) -> typing.Self: return self.operator(operator = operator.abs)
 
-	def __eq__(self, other: object) -> bool: return self.comparison(other, float.__eq__)
-	def __le__(self, other: typing.Self) -> bool: return self.comparison(other, float.__le__)
-	def __ge__(self, other: typing.Self) -> bool: return self.comparison(other, float.__ge__)
-	def __lt__(self, other: typing.Self) -> bool: return self.comparison(other, float.__lt__)
-	def __gt__(self, other: typing.Self) -> bool: return self.comparison(other, float.__gt__)
+	def __eq__(self, other: object     , /) -> bool: return self.relation(other, relation = operator.eq)
+	def __le__(self, other: typing.Self, /) -> bool: return self.relation(other, relation = operator.le)
+	def __ge__(self, other: typing.Self, /) -> bool: return self.relation(other, relation = operator.ge)
+	def __lt__(self, other: typing.Self, /) -> bool: return self.relation(other, relation = operator.lt)
+	def __gt__(self, other: typing.Self, /) -> bool: return self.relation(other, relation = operator.gt)
 
 	@classmethod
 	def cast(cls, value: typing.Self | float) -> typing.Self:
@@ -139,23 +141,24 @@ class Float:
 		)
 
 	@typing.overload
-	def operation(self, /, *, operation: typing.Callable[[float], float]) -> typing.Self: ...
+	def operator(self, /, *, operator: typing.Callable[[float], float]) -> typing.Self: ...
 
 	@typing.overload
-	def operation(self, other: typing.Self, /, *, operation: typing.Callable[[float, float], float]) -> typing.Self: ...
+	def operator(self, other: typing.Self, /, *, operator: typing.Callable[[float, float], float]) -> typing.Self: ...
 
-	def operation(self, other: typing.Self | None = None, /, *, operation: typing.Callable[..., float]) -> typing.Self:
-		if other is None:
-			return self.cast(operation(self._))
+	def operator(self, *others: typing.Self, operator: typing.Callable[..., float]) -> typing.Self:
+		if not others:
+			return self.cast(operator(self._))
+
+		other, *_ = others
 
 		if type(other) is not type(self):
 			return NotImplemented
 
-		return self.cast(operation(self._, other._))
+		return self.cast(operator(self._, other._))
 
-	def comparison(self, other: object, comparison: typing.Callable[[float, float], bool]) -> bool:
+	def relation(self, other: object, relation: typing.Callable[[float, float], bool]) -> bool:
 		if type(other) is not type(self):
 			return NotImplemented
 
-		other = typing.cast(typing.Self, other)
-		return comparison(self._, other._)
+		return relation(self._, other._)
