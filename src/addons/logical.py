ece7a831a81ca9699927c4fa3b1678f654f8cfa7
Stratos Papadoudis <stratos.papadoudis@helvia.ai>
2026-08-06 15:34:39 +0300

    Close `real`/`dist`/`prob` algebra

--- src/addons/logical.py
+++ src/addons/logical.py
@@ -15,13 +15,14 @@ class base(float):
 	minimum: float
 	midimum: float
 	maximum: float
-	optimum: float
 
 	def __new__(cls, value: numeric | logical, /) -> typing.Self:
 		self = super().__new__(cls, getattr(value, cls.__name__.lower(), value))
 
-		if not cls.minimum <= float(self) <= cls.maximum:
-			raise ValueError(f"not {cls.minimum} <= {self} <= {cls.maximum} for {cls.__name__}")
+		lower, upper = sorted(cls.range())
+
+		if not lower <= float(self) <= upper:
+			raise ValueError(f"not {lower} <= {self} <= {upper} for {cls.__name__}")
 
 		return self
 
@@ -29,71 +30,66 @@ class base(float):
 #	def     __sub__(self, value: float, /) -> typing.Self: return self.__class__(super().    __sub__(value))
 	def     __mul__(self, value: float, /) -> typing.Self: return self.__class__(super().    __mul__(value))
 	def __truediv__(self, value: float, /) -> typing.Self: return self.__class__(super().__truediv__(value))
-	def     __mod__(self, value: float, /) -> typing.Self: return self.__class__(super().    __mod__(value))
+#	def     __mod__(self, value: float, /) -> typing.Self: return self.__class__(super().    __mod__(value))
 
-	def     __and__(self, value: float, /) -> typing.Self: return self.__class__(min(self, value))
-	def      __or__(self, value: float, /) -> typing.Self: return self.__class__(max(self, value))
+#	def __floordiv__(self, value: float, /) -> typing.Self:
+#		return self.__class__(super().__floordiv__(value))
 
-	def __floordiv__(self, value: float, /) -> typing.Self:
-		return self.__class__(super().__floordiv__(value))
+	def __and__(self, value: float, /) -> typing.Self: return self.__class__(self.prob.meet(self.__class__(value).prob))
+	def  __or__(self, value: float, /) -> typing.Self: return self.__class__(self.prob.join(self.__class__(value).prob))
 
 	def __pow__(self, value: float, mod: None = None, /) -> typing.Self:
 		return self.__class__(super().__pow__(value, mod))
 
-	def __divmod__(self, value: float, /) -> pair[typing.Self]:
-		quotient, remainder = super().__divmod__(value)
-
-		return (
-			self.__class__(quotient ),
-			self.__class__(remainder),
-		)
-
 	def __sub__(self, value: float, /) -> typing.Self:
-		return self & ~self.__class__(value)
+		value = self.__class__(value)
+
+		return self & ~value
 
 	def __xor__(self, value: float, /) -> typing.Self:
-		return (self - value) | (self.__class__(value) - self)
-		return (self | value) - (self.__class__(value) & self)
+		value = self.__class__(value)
 
-	def     __radd__(self, value: float, /) -> typing.Self: return self.__class__(value) + self
-	def     __rsub__(self, value: float, /) -> typing.Self: return self.__class__(value) - self
-	def     __rmul__(self, value: float, /) -> typing.Self: return self.__class__(value) * self
-	def __rtruediv__(self, value: float, /) -> typing.Self: return self.__class__(value) / self
-	def     __rmod__(self, value: float, /) -> typing.Self: return self.__class__(value) % self
-	def     __rand__(self, value: float, /) -> typing.Self: return self.__class__(value) & self
-	def      __ror__(self, value: float, /) -> typing.Self: return self.__class__(value) | self
-	def     __rxor__(self, value: float, /) -> typing.Self: return self.__class__(value) ^ self
+		return (self - value) | (value - self)
+		return (self | value) - (value & self)
+
+	def      __radd__(self, value: float, /) -> typing.Self: return self.__class__(value) +  self
+	def      __rsub__(self, value: float, /) -> typing.Self: return self.__class__(value) -  self
+	def      __rmul__(self, value: float, /) -> typing.Self: return self.__class__(value) *  self
+	def  __rtruediv__(self, value: float, /) -> typing.Self: return self.__class__(value) /  self
+#	def      __rmod__(self, value: float, /) -> typing.Self: return self.__class__(value) %  self
+#	def __rfloordiv__(self, value: float, /) -> typing.Self: return self.__class__(value) // self
+	def      __rand__(self, value: float, /) -> typing.Self: return self.__class__(value) &  self
+	def       __ror__(self, value: float, /) -> typing.Self: return self.__class__(value) |  self
+	def      __rxor__(self, value: float, /) -> typing.Self: return self.__class__(value) ^  self
 
 #	def __rpow__(self, value: float, mod: None = None, /) -> typing.Self:
-#		return self.__class__(super().__rpow__(value, mod))
-
-#	def __rdivmod__(self, value: float, /) -> pair[typing.Self]:
-#		quotient, remainder = super().__rdivmod__(value)
-#
-#		return (
-#			self.__class__(quotient ),
-#			self.__class__(remainder),
-#		)
+#		return self.__class__(value).__pow__(self, mod)
 
 	def __neg__(self, /) -> typing.Self: return self.__class__(super().__neg__())
 	def __pos__(self, /) -> typing.Self: return self.__class__(super().__pos__())
 	def	__abs__(self, /) -> typing.Self: return self.__class__(super().__abs__())
 
 	def __invert__(self, /) -> typing.Self:
-		return -self
+		return self.__class__(self.prob.complement())
 
-#	@classmethod
-#	def range(cls, /) -> pair[float]:
-#		return (
-#			min(
-#				cls.minimum,
-#				cls.maximum,
-#			),
-#			max(
-#				cls.minimum,
-#				cls.maximum,
-#			),
-#		)
+	def __eq__(self, value: float, /) -> prob:
+		meet = (self & value).prob
+		join = (self | value).prob
+
+		return prob(meet / join if join else prob.minimum)
+
+	@classmethod
+	def range(cls, /) -> pair[float]:
+		return (
+			min(
+				cls.minimum,
+				cls.maximum,
+			),
+			max(
+				cls.minimum,
+				cls.maximum,
+			),
+		)
 
 	@property
 	def real(self, /) -> real: return real(self)
@@ -110,10 +106,9 @@ class base(float):
 
 class real(base):
 
-	minimum: float = -math.inf
+	minimum: float = +math.inf
 	midimum: float =     0.
-	maximum: float = +math.inf
-	optimum: float = maximum
+	maximum: float = -math.inf
 
 	@property
 	def real(self, /) -> real:
@@ -133,23 +128,13 @@ class dist(base):
 	minimum: float =     0.
 	midimum: float =     1.
 	maximum: float = +math.inf
-	optimum: float = minimum
 
 	def __mul__(self, value: float, /) -> typing.Self:
 		return self.__class__(self.prob * self.__class__(value).prob)
 
-	def __and__(self, value: float, /) -> typing.Self:
-		return self.__class__(self.prob & self.__class__(value).prob)
-
-	def  __or__(self, value: float, /) -> typing.Self:
-		return ~(~self & ~self.__class__(value))
-
-	def __neg__(self, /) -> typing.Self:
-		return self.__class__(~self.prob)
-
 	@property
 	def real(self, /) -> real:
-		return real(-math.log(float(self)) if self else real.maximum)
+		return real(-math.log(float(self)) if self else real.minimum)
 
 	@property
 	def dist(self, /) -> dist:
@@ -163,24 +148,13 @@ class dist(base):
 
 class prob(base):
 
-	minimum: float =    0.
+	minimum: float =    1.
 	midimum: float = math.exp(-1)
 #	midimum: float =     .5
-	maximum: float =    1.
-	optimum: float = maximum
+	maximum: float =    0.
 
 	def __add__(self, value: float, /) -> typing.Self:
 		return self.__class__(self.dist + self.__class__(value).dist)
-
-	def __and__(self, value: float, /) -> typing.Self:
-		return self * value
-
-	def  __or__(self, value: float, /) -> typing.Self:
-		return ~(~self & ~self.__class__(value))
-
-	def __neg__(self, /) -> typing.Self:
-		return self.__class__(1 - self)
-
 
 	@property
 	def real(self, /) -> real:
@@ -194,6 +168,17 @@ class prob(base):
 	@property
 	def prob(self, /) -> prob:
 		return self
+
+	def meet(self, value: float, /) -> typing.Self:
+		return self * self.__class__(value)
+
+	def join(self, value: float, /) -> typing.Self:
+		other = self.__class__(value)
+
+		return self.complement().meet(other.complement()).complement()
+
+	def complement(self, /) -> typing.Self:
+		return self.__class__(self.minimum - float(self))
 
 
 logical: typing.TypeAlias = real | dist | prob
@@ -209,7 +194,7 @@ class indexset[T: typing.Hashable](dict[T, bool]):
 		self.default = default
 
 	def __repr__(self, /) -> str:
-		return f"{self.__class__.__name__}({super().__repr__()}, default={self.default!r})"
+		return repr(set(self)) if not self.default else "~" + repr(set(~self))
 
 	def __missing__(self, _: T, /) -> bool:
 		return self.default
